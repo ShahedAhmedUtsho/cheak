ba94c1e88d2921e6e695d88bf8d41dc479750351
Shahed Ahmed <shahedahmedutsho@gmail.com>
2024-06-02 23:21:55 +0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const cors = require("cors");
+require('dotenv').config()
+app.use(cors());
+app.use(express.json())
+
+
+
+
+
+
+
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.swwr6sg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+  
+        await client.connect();
+
+        
+
+
+        
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send("hello from my server")
+
+})
+
+app.listen(port, () => {
+    console.log(`my first server running on port ${port}`)
+})
